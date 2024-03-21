ac594e64d9e8ab00a6eee7e95ce5539225cba2fa
albab83 <141700905+albab83@users.noreply.github.com>
2024-03-21 22:16:52 +0700

    yaa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+class Button extends React.Component {
+  render() {
+    return <div>
+       <button
+    type="button"
+    className={`inline-block rounded ${this.props.varian} px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong`}>
+    {this.props.children}
+  </button>
+
+    </div>
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center bg-green-700 min-h-screen items-center gap-3">
+      <Button varian="bg-slate-700">login</Button>
+      <Button varian="bg-red-700">logout</Button>
+      <Button varian="bg-blue-700">buy now</Button>
+      <Button varian="bg-yellow-700">add to cart</Button>
     </div>
   );
 }
